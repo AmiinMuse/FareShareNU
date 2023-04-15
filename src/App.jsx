b548dfae7a905a923be7c97bfd23f1103dd790e7
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2023-04-15 15:08:19 -0500

    Added HTML for Help page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Home from './pages/Home'
 
 import './App.css'
+import Help from './pages/help'
 
 const AppLayout = () => {
   return (
@@ -16,13 +17,13 @@ function App() {
     <div className="App">
       {/* <Routes> */}
         {/* <Route path="/" element={<AppLayout/>}>
-          <Route index element={<Home/>}/>
+          <Route index element={<Help/>}/>
           <Route path="create" element={<CreateCrewmate/>}/>
           <Route path="gallery" element={<CrewmateGallery/>}/>
           <Route path="edit/:id" element={<EditCrewmate/>}/>
         </Route> */}
       {/* </Routes> */}
-      <Home/>
+      <Help/>
     </div>
   )
 }
---
+++ src/pages/Help.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import './Help.css'
+
+const Help = () => {
+    return (
+    <div>
+        <div>
+            <title>Help Page</title>
+        </div>
+        <div>
+            <h1>Help Page</h1>
+            <p>Welcome to our help page! Here, you can find answers to commonly asked questions and solutions to common problems. If you can't find what you're looking for, please don't hesitate to contact us.</p>
+            <h2>Frequently Asked Questions</h2>
+            <ul>
+                <li>How do I create an account?</li>
+                <li>What should I do if I forget my password?</li>
+                <li>How do I update my personal information?</li>
+                <li>What payment methods do you accept?</li>
+                <li>How can I track my order?</li>
+            </ul>
+            <h2>Contact Us</h2>
+            <p>If you have any questions or concerns, please contact us:</p>
+            <ul>
+                <li>Phone: 1-800-123-4567</li>
+                <li>Email: support@yourwebsite.com</li>
+                <li>Live chat: Click the "Chat Now" button in the bottom right corner</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Help
