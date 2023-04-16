cec8c9a93f0475ed788e1317dbaed6c61b163eaf
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2023-04-16 09:36:39 -0500

    GroupOverview updates

--- src/pages/GroupsOverview.jsx
+++ src/pages/GroupsOverview.jsx
@@ -4,7 +4,30 @@ import './GroupsOverview.css'
 
 const GroupsOverview = () => {
     return (
-        <h1>Groups Overview</h1>
+        <div className="table_test">
+            <table>
+                <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                </tr>
+                <tr>
+                <td>Anom</td>
+                <td>19</td>
+                <td>Male</td>
+                </tr>
+                <tr>
+                <td>Megha</td>
+                <td>19</td>
+                <td>Female</td>
+                </tr>
+                <tr>
+                <td>Subham</td>
+                <td>25</td>
+                <td>Male</td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,19 @@ import './Profile.css'
 
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+            <div>
+                <div class="profile">
+                    <h1 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Profile</h1>
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="Profile Picture"/>
+                    <h1>John Doe</h1>
+                    <h2>Web Developer</h2>
+                    <ul>
+                        <li><strong>Email:</strong> john.doe@email.com</li>
+                        <li><strong>Phone:</strong> 123-456-7890</li>
+                        <li><strong>Website:</strong> www.johndoe.com</li>
+                    </ul>
+                </div>
+            </div>
     )
 }
 
